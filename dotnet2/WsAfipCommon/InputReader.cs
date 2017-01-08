6dfed9827ccdd3d5aef4d34ebc3d521432dc4886
Leonardo Helman <leonardohelman@yahoo.com.ar>
2017-01-08 18:42:54 -0300

    Adding FECompConsultar

---
+++ dotnet2/WsAfipCommon/InputReader.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Xml;
+
+public class InputReader<WsfeRequestType>
+{
+    private bool _verboseMode;
+    public bool verbose { set { _verboseMode = value; } }
+    WsfeRequestType feReq;
+    public InputReader(WsfeRequestType feReq)
+    {
+        feReq = this.feReq;
+    }
+
+    public WsfeRequestType leeInputEnXml(string archivo)
+    {
+        System.Xml.Serialization.XmlSerializer reader = new System.Xml.Serialization.XmlSerializer(feReq.GetType());
+
+        // Read the XML file.
+        System.IO.StreamReader file =
+           new System.IO.StreamReader(archivo);
+
+        // Deserialize the content of the file into a Book object.
+        feReq = (WsfeRequestType)reader.Deserialize(file);
+        
+        return feReq;
+    }
+}
